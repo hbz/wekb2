23d40860faf4ca807f2751c8365ba4c1585118a8
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2023-01-05 14:33:14 +0100

    add files directory

---
+++ files/server/wekb-config.groovy
@@ -0,0 +1,34 @@
+
+println("------- Using local config from ~/.grails/wekb-config.groovy ------- ")
+
+grails.mail.host = 'localhost'
+grails.mail.port = 25
+
+deployBackupLocation = ''
+pgDumpPath = ''
+
+dataSource.username = 'gokb'
+dataSource.password = 'gokb'
+dataSource.url = 'jdbc:postgresql://localhost:5432/gokb'
+
+serverUrl= 'http://localhost:8080/gokb'
+baseUrl= 'http://localhost:8080/gokb'
+server.contextPath = '/wekb'
+
+systemId = 'we:kb-Dev'
+
+logSql = true
+formatSql = true
+
+wekb.anonymizeUsers = false
+wekb.defaultCuratoryGroup = 'Local'
+wekb.es.cluster = 'gokbes'
+wekb.es.host = 'localhost'
+wekb.ftupdate_enabled = false
+wekb.languagesUrl = 'localhost/languages'
+wekb.packageUpdate.enabled = false
+wekb.enable_statsrewrite = false
+wekb.sendJobInfosJob = false
+wekb.kbartImportStorageLocation = '/tmp/kbartImport'
+
+
