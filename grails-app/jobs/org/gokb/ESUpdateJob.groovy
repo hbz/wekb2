66a4599038b8a2636df89dae43f0a8b0d59e2672
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2023-01-09 10:49:37 +0100

    add jobs

---
+++ grails-app/jobs/org/gokb/ESUpdateJob.groovy
@@ -0,0 +1,25 @@
+package org.gokb
+
+class ESUpdateJob {
+
+  // Allow only one run at a time.
+  static concurrent = false
+
+  def FTUpdateService
+
+  static triggers = {
+    // Cron timer.
+    cron name: 'ESUpdateTrigger', cronExpression: "0 0/5 * * * ?", startDelay:500000
+  }
+
+  def execute() {
+    if ( grailsApplication.config.wekb.ftupdate_enabled ) {
+      log.info ("Beginning scheduled es update job.")
+      FTUpdateService.updateFTIndexes()
+      log.info ("ESUpdateJob completed.")
+    }
+    else {
+      log.info("FTUpdate is not enabled - set config.ftupdate_enabled = true in config to enable")
+    }
+  }
+}
