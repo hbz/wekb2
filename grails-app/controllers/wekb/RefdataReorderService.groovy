010a9077702751cb20cef17d4e7e1dd75e371f6b
galffy <andreas.galffy@hbz-nrw.de>
2025-01-23 15:40:03 +0100

    various minor fixes for for ERMS-6135 and ERMS-6138

--- grails-app/controllers/wekb/RefdataReorderService.groovy
+++ grails-app/controllers/wekb/RefdataReorderService.groovy
@@ -37,14 +37,22 @@ class RefdataReorderService {
 
         //Federations: ACOnet, DFN, SWITCH, eduGAIN, other, none, rest
         RefdataCategory platformFederation = RefdataCategory.findByDesc(RCConstants.PLATFORM_FEDERATION)
-        RefdataValue.executeUpdate('update RefdataValue rdv set rdv.order = 0 where rdv.owner = :federation and rdv.value = :aconet', [federation: platformFederation, aconet: 'ACOnet Identity Federation'])
-        RefdataValue.executeUpdate('update RefdataValue rdv set rdv.order = 10 where rdv.owner = :federation and rdv.value = :dfn', [federation: platformFederation, dfn: 'DFN-AAI'])
-        RefdataValue.executeUpdate('update RefdataValue rdv set rdv.order = 20 where rdv.owner = :federation and rdv.value = :switch', [federation: platformFederation, switch: 'SWITCHaai'])
-        RefdataValue.executeUpdate('update RefdataValue rdv set rdv.order = 30 where rdv.owner = :federation and rdv.value = :edugain', [federation: platformFederation, edugain: 'eduGAIN'])
-        Set<String> specialFederations = ['ACOnet Identity Federation', 'DFN-AAI', 'SWITCHaai', 'eduGAIN']
         order = 40
-        RefdataValue.findAllByValueNotInListAndOwner(specialFederations, platformFederation).eachWithIndex { RefdataValue federation, int i ->
-            federation.order = i*10+order
+        int i = 0
+        RefdataValue.findAllByOwner(platformFederation).each { RefdataValue federation ->
+            switch(federation.value) {
+                case 'ACOnet Identity Federation': federation.order = 0
+                    break
+                case 'DFN-AAI': federation.order = 10
+                    break
+                case 'SWITCHaai': federation.order = 20
+                    break
+                case 'eduGAIN': federation.order = 30
+                    break
+                default: federation.order = i*10+order
+                    i++
+                    break
+            }
             federation.save()
         }
     }
