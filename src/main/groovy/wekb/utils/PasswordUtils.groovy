885057a858db99d4789d997f6b9fefc4ed594d15
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2025-08-27 12:30:14 +0200

    various bugfixes includes commits for: - ERMS-6171: correct processing of status queries - no ticket password bugfix

--- src/main/groovy/wekb/utils/PasswordUtils.groovy
+++ src/main/groovy/wekb/utils/PasswordUtils.groovy
@@ -11,7 +11,7 @@ class PasswordUtils {
 
     public static final String USER_PASSWORD_INFO =
             "The password must be between 8 and 20 characters long." +
-                    "It must contain at least one uppercase letter, one lowercase letter, one number and one special character from the list ( !\$%&@#^+*~=:; )." +
+                    "It must contain at least one uppercase letter, one lowercase letter, one number and one special character from the list ( !\$%&@#^+*~=:;- )." +
                     "Spaces are not allowed."
 
     public static final String USER_PASSWORD_REGEX =
@@ -19,7 +19,7 @@ class PasswordUtils {
                     '(?=.*[A-Z])' +             // an upper case alphabet that must occur at least once
                     '(?=.*[a-z])' +             // a lower case alphabet must occur at least once
                     '(?=.*[0-9])' +             // a digit must occur at least once
-                    '(?=.*[!$%&@#^+*~=:;])' +   // a special character that must occur at least once: !$%&@#^+*~=:;
+                    '(?=.*[!$%&@#^+*~=:;-])' +   // a special character that must occur at least once: !$%&@#^+*~=:;
                     '(?=\\S+$)' +               // white spaces are not allowed
                     '.{8,20}' +                 // 8 - 20 characters
                     '$'
