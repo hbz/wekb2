17f77dcf717472523f9e6442c079b0777b1836d8
MDjeb <moetez.djebeniani@hbz-nrw.de>
2025-12-11 13:06:00 +0100

    ERMS-7010: Anfragen mit unterschiedlichen DB-User bearbeiten

--- files/server/tenantSetup.sql
+++ files/server/tenantSetup.sql
@@ -11,3 +11,6 @@ GRANT wekb_with_login TO db_user;
 -- Grants for tenant users
 GRANT ALL ON ALL TABLES IN SCHEMA "public" TO wekb_with_login;
 GRANT SELECT ON ALL TABLES IN SCHEMA "public" TO wekb_without_login;
+
+
+ALTER ROLE wekb_without_login set statement_timeout TO 60000;
