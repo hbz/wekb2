ae99bffdd61c1bf27d7f4984abc582bfa104e91b
bluoss <ingrid.bluoss@hbz-nrw.de>
2026-09-01 18:06:28 +0200

    ERMS-7522

--- grails-app/assets/javascripts/modules/newsFeedCutter.js
+++ grails-app/assets/javascripts/modules/newsFeedCutter.js
@@ -40,7 +40,7 @@ const newsFeedCutter = {
 
                 const articleBottom = this
                     .getBoundingClientRect()
-                    .bottom+100;
+                    .bottom+125;
 
                 if (articleBottom > newsColumnBottom) {
                     $article.hide();
