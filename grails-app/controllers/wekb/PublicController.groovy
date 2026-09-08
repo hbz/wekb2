1109872c9bed4e42f4bd0fc24adda9a7b4b89118
bluoss <ingrid.bluoss@hbz-nrw.de>
2026-09-08 16:56:26 +0200

    ERMS-7543

--- grails-app/controllers/wekb/PublicController.groovy
+++ grails-app/controllers/wekb/PublicController.groovy
@@ -242,11 +242,11 @@ class PublicController {
 
     result.dateNow = new Date()
 
-    Date dateFor6Years = Date.from(LocalDate.now().minusDays(2190).atStartOfDay(ZoneId.systemDefault()).toInstant())
+    Date dateFor14Days = Date.from(LocalDate.now().minusDays(14).atStartOfDay(ZoneId.systemDefault()).toInstant())
 
     SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd")
 
-    result.dateFor6Years =  format.format(dateFor6Years)
+    result.dateFor14Days =  format.format(dateFor14Days)
 
     List newsAboutObjects = [
                              'Org',
@@ -263,9 +263,9 @@ class PublicController {
       result.news[domainClassName.toLowerCase()] = [:]
 
       String queryNew = "from ${domainClassName} where  status in (:status) and dateCreated >= :daysBefore order by dateCreated desc"
-      result.news[domainClassName.toLowerCase()] .newInDB = Package.executeQuery(queryNew, [status: status, daysBefore: dateFor6Years], [max: 50, offset: 0])
+      result.news[domainClassName.toLowerCase()] .newInDB = Package.executeQuery(queryNew, [status: status, daysBefore: dateFor14Days], [max: 50, offset: 0])
       String queryLastUpdated = "from ${domainClassName} where TO_CHAR(dateCreated,'YYYY-MM-DD') != TO_CHAR(lastUpdated,'YYYY-MM-DD') and status in (:status) and lastUpdated >= :daysBefore order by lastUpdated desc"
-      result.news[domainClassName.toLowerCase()] .lastUpdatedInDB = Package.executeQuery(queryLastUpdated, [status: status, daysBefore: dateFor6Years], [max: 50, offset: 0])
+      result.news[domainClassName.toLowerCase()] .lastUpdatedInDB = Package.executeQuery(queryLastUpdated, [status: status, daysBefore: dateFor14Days], [max: 50, offset: 0])
 
     }
     List<Map> allNews = []
